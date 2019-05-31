054d75ff8a974c7729d9a8e52f41e2835f9712de
Simon De Backer <simondebacker@msn.com>
2019-05-31 22:19:40 +0200

    init

---
+++ QT_AVR/include/MCP2515_CAN.h
@@ -0,0 +1,36 @@
+/* file : MCP2515_CAN.h
+ * doel : can bus
+ * 20190527 : init
+ *
+ */
+
+/**
+  * \code
+  *
+  *
+  * \endcode
+*/
+
+#ifndef MCP2515_CAN_h
+#define MCP2515_CAN_h
+
+/* Includes: */
+
+
+/* Enable C linkage for C++ Compilers: */
+    #if defined(__cplusplus)
+        extern "C" {
+    #endif
+
+/* Defines */
+
+/* Inline Functions: */
+
+/* Disable C linkage for C++ Compilers: */
+    #if defined(__cplusplus)
+        }
+    #endif
+
+#endif
+
+        /** @} */
